a2e04302a582e7f476267e037b3c26dc9ea99dd8
7heIVIaze <kimdave20@naver.com>
2022-06-24 18:06:49 +0900

    knu wiki ver 0.1.0 add document ACL

---
+++ middleware/canAnybodyRevert.js
@@ -0,0 +1,20 @@
+const doc_version_Model = require('../models/document')
+
+module.exports = async (req, res, next) => {
+    const url = req.url // /revert/문서명(encoded)
+    const version = req.query.rev // 버전값
+    const parsedurl = url.split('/') // '', revert, 문서명(encoded)
+    const search = decodeURI(parsedurl[2]) // 문서명(decoded) => 문서명?rev=버전값
+    const parsedsearch = search.split('?') // 문서명, rev=버전값
+    const docName = parsedsearch[0] // 문서명
+
+    const document = await doc_version_Model.findOne({wiki_doc_title: docName}).sort({"_id": -1}).limit(1)
+    // 편집제한 문서인지 확인하기 위해 최근에 저장된 데이터 가져옴
+
+    if(document.canAnybodyWrite && req.user == null) { // 로그인 안한 이용자가 편집제한 문서를 편집하려할 때
+        if(version) return res.redirect(`/w/${docName}?version=${version}`)
+        else if(!version) return res.redirect(`/w/${docName}`)
+        else return res.redirect('/')
+    }
+    next()
+}
