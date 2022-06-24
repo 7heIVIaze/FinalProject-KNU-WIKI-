ff435ea543f20af6f68fb5b81142a6642e4faa20
7heIVIaze <kimdave20@naver.com>
2022-06-24 21:21:19 +0900

    knu wiki ver 0.1.0 add document ACL

--- middleware/canAnybodyWrite.js
+++ middleware/canAnybodyWrite.js
@@ -7,9 +7,11 @@ module.exports = async (req, res, next) => {
 
     const document = await doc_version_Model.findOne({wiki_doc_title: search}).sort({"_id": -1}).limit(1)
 
-    if(!document.canAnybodyWrite && req.user == null) { // 로그인 안한 이용자가 편집제한 문서를 편집하려할 때
-        if(search) return res.redirect(`/w/${search}`)
-        else return res.redirect('/')
+    if(document) { // 문서가 있을 때 확인함 (Revert의 경우에는 문서가 있는 것을 되돌리는 것이기 때문에 확인 안 함)
+        if(document.canAnybodyWrite && req.user == null) { // 로그인 안한 이용자가 편집제한 문서를 편집하려할 때
+            if(search) return res.redirect(`/w/${search}`)
+            else return res.redirect('/')
+        }
     }
     next()
 }
