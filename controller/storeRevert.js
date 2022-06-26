84dc4fe6ede2a9b340c9e97a5b919d4394cc89b6
7heIVIaze <kimdave20@naver.com>
2022-06-26 15:31:52 +0900

    knu wiki ver 0.1.0

--- controller/storeRevert.js
+++ controller/storeRevert.js
@@ -7,6 +7,9 @@ module.exports = async (req, res) => {
     const search = decodeURI(parsedurl[2]) // 문서명(decoded) => 문서명?version=버전값
     const parsedsearch = search.split('?') // 문서명, version=버전값
     const docName = parsedsearch[0] // 문서명
+    let writer = requestIp.getClientIp(req) // 작성자는 일단 해당 작성자의 ip
+    if(req.user) writer = req.user.username // 만약 로그인 했다면 아이디로
+
     
     const newversion = await doc_version_Model.findOne({wiki_doc_title: docName}).sort({"_id": -1}).limit(1)
     const document = await doc_version_Model.findOne({wiki_doc_title: docName, version: version})
@@ -15,6 +18,7 @@ module.exports = async (req, res) => {
         body: document.body,
         version: newversion.version + 1,
         wiki_doc_title: docName,
+        writer: writer,
     })
     res.redirect(`/w/${docName}`)
 }
